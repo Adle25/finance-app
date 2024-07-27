abd51836c287bb1b59ae1656e15944406524d509
Adle25 <askaradilet41@gmail.com>
2024-07-27 19:49:35 +0500

    Added Hono for routes

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,27 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { z } from 'zod'
+import { zValidator } from '@hono/zod-validator'
+
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app
+    .get('/hello', (c) => {
+        return c.json({
+            message: 'Hello, Next.js!'
+        })
+    })
+    .get('/hello/:test', (c) => {
+        const test = c.req.param('test');
+
+        return c.json({
+            message: 'Hello, World',
+            test: test
+        })
+    })
+
+export const GET = handle(app)
+export const POST = handle(app)
