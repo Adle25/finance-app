11bbc2c417600d045553d819e607e23c92064d22
Adle25 <askaradilet41@gmail.com>
2024-07-29 19:31:25 +0500

    Added GET accounts endpoint

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+"use client";
 
 export default function Home() {
   return (
-    <UserButton />
-  );
+    <div>Home Page</div>
+  )
 }
