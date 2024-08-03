663f57a321440dd505d0c969fac59207a2aff603
Adle25 <askaradilet41@gmail.com>
2024-08-03 23:54:39 +0500

    Added upload button

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -9,9 +9,23 @@ import { DataTable } from "@/components/data-table";
 import { useGetTransactions } from "@/features/transactions/api/use-get-transactions";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
+import { useState } from "react";
+import { UploadButton } from "./upload-button";
 
+
+enum VARIANTS {
+    LIST = "LIST",
+    IMPORT = "IMPORT"
+};
+
+const INITIAL_IMPORT_RESULTS = {
+    data: [],
+    errors: [],
+    meta: {}
+};
 
 const TransactionsPage = () => {
+    const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST)
     const transactionsQuery = useGetTransactions();
     const deleteTransactions = useBulkDeleteTransactions();
     const newTransaction = useNewTransaction();
@@ -35,6 +49,16 @@ const TransactionsPage = () => {
         )
     }
 
+    if (variant === VARIANTS.IMPORT) {
+        return (
+            <>
+                <div>
+                    This is a screen for import
+                </div>
+            </>
+        );
+    }
+
     return (
         <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
             <Card className="border-none drop-shadow-sm">
@@ -44,6 +68,7 @@ const TransactionsPage = () => {
                         <Plus className="size-4 mr-2" />
                         Add new
                     </Button>
+                    <UploadButton onUpload={() => { }} />
                 </CardHeader>
                 <CardContent>
                     <DataTable columns={columns} data={transactions} filterKey="payee" disabled={isDisabled} onDelete={(row) => {
