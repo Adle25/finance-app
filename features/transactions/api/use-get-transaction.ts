35430cadb2207a8eb3e29795f8c3385a70a4201a
Adle25 <askaradilet41@gmail.com>
2024-08-05 23:03:32 +0500

    Added summary api and data to db

--- features/transactions/api/use-get-transaction.ts
+++ features/transactions/api/use-get-transaction.ts
@@ -1,5 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { client } from "@/lib/hono"
+import { converAmountFromMilliUnits } from "@/lib/utils"
 
 
 export const useGetTransaction = (id?: string) => {
@@ -17,7 +18,10 @@ export const useGetTransaction = (id?: string) => {
 
             const { data } = await response.json()
 
-            return data
+            return {
+                ...data,
+                amount: converAmountFromMilliUnits(data.amount)
+            }
         }
     })
 
